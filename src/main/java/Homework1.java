11f7ef704938d60d96edea12bc0184c880f3ea12
olicze <aleksandradziedziuch@gmail.com>
2020-11-14 21:17:32 +0100

    The first Homework1 is added.

--- src/main/java/Homework1.java
+++ src/main/java/Homework1.java
@@ -1,7 +1,35 @@
 public class Homework1 {
 
     public static void main(String[] args) {
-        System.out.println("Here I am");
-    }
 
+
+        // initializing four variables with random values
+
+        int a = 3;
+        int b = 1;
+        int c = 12;
+        int d = 6;
+
+        // checking the sum of the first and second variables
+
+        int sum1 = a + b;
+        if (sum1 % 2 == 0) {
+            System.out.println("The sum of the first pair is an even number (" + sum1 + ").");
+        }
+
+        // checking the sum of the third and fourth variables
+
+        int sum2 = c + d;
+        if (sum2 % 2 == 0) {
+            System.out.println("The sum of the second pair is an even number (" + sum2 + ").");
+        }
+
+        // displaying the results when the two sums are an even number
+
+        int sum = sum1 + sum2;
+        if (sum1 % 2 == 0 && sum2 % 2 == 0) {
+            System.out.println("The sum of all variables is an even number (" + sum + ").");
+        }
+    }
 }
+
